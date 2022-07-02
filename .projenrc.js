924dcc92c39bd3c14ab7d1fe256a57ce220ebb60
Dean Foran <dean.foran@zeeklabs.co.uk>
2022-07-02 11:10:30 +0100

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,13 @@
+const { typescript } = require('projen');
+const project = new typescript.TypeScriptAppProject({
+  defaultReleaseBranch: 'main',
+  github: true,
+  name: 'cqrs-ts',
+  package: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
