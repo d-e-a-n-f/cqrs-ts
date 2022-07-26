507ff22ebb15bcc5530621887a68cba7f629a46c
Dean Foran <dean.foran@kes.global>
2022-07-26 14:05:55 +0100

    cleanup projen

--- .projenrc.js
+++ .projenrc.js
@@ -4,14 +4,8 @@ const project = new typescript.TypeScriptAppProject({
   github: true,
   name: 'cqrs-ts',
   package: true,
-  npmTokenSecret: 'CQRS_TS_NPM_TOKEN',
   release: true,
   mutableBuilds: false,
   releaseToNpm: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
